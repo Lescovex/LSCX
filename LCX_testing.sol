3c52e49792cbff68aaad0d991b6b19af7b9ba6be
nOsDelAbismo <srdelabismo@lescovex.com>
2018-03-04 11:31:12 +0100

    reward policy changes

--- LCX_testing.sol
+++ LCX_testing.sol
@@ -62,12 +62,24 @@ contract Ownable {
 //////////////////////////////////////////////////////////////
 
 contract LescovexERC20 is Ownable {
+    
     using SafeMath for uint256;
 
-    mapping (address => uint256) holded;
-    mapping (address => uint256) balances;
+
+    mapping (address => uint256) public balances;
+
     mapping (address => mapping (address => uint256)) internal allowed;
 
+    mapping (address => timeHold) holded;
+
+    struct timeHold{
+        uint256[] amount;
+        uint256[] time;
+        uint256 length;
+    }
+
+
+    
     uint256 public constant blockEndICO = 1524182460;
 
     /* Public variables for the ERC20 token */
@@ -80,9 +92,8 @@ contract LescovexERC20 is Ownable {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    function holdedOf(address _owner) public view returns (uint256 balance) {
-        return holded[_owner];
-    }
+
+
 
     function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
@@ -95,7 +106,14 @@ contract LescovexERC20 is Ownable {
 
         // SafeMath.sub will throw if there is not enough balance.
         balances[msg.sender] = balances[msg.sender].sub(_value);
-        holded[_to] = block.number;
+        
+
+        delete holded[msg.sender];
+
+        holded[_to].amount.push(_value);
+        holded[_to].time.push(block.number);
+        holded[_to].length++;
+        
         balances[_to] = balances[_to].add(_value);
 
         Transfer(msg.sender, _to, _value);
@@ -107,7 +125,13 @@ contract LescovexERC20 is Ownable {
         require(_value <= balances[_from]);
         require(_value <= allowed[_from][msg.sender]);
         balances[_from] = balances[_from].sub(_value);
-        holded[_to] = block.number;
+        
+        delete holded[msg.sender];
+
+        holded[_to].amount.push(_value);
+        holded[_to].time.push(block.number);
+        holded[_to].length++;
+
         balances[_to] = balances[_to].add(_value);
         allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
 
@@ -116,7 +140,7 @@ contract LescovexERC20 is Ownable {
     }
 
 
-    function approve(address _spender, uint256 _value) public onlyOwner returns (bool) {
+    function approve(address _spender, uint256 _value) public returns (bool) {
         allowed[msg.sender][_spender] = _value;
         Approval(msg.sender, _spender, _value);
         return true;
@@ -126,13 +150,13 @@ contract LescovexERC20 is Ownable {
         return allowed[_owner][_spender];
     }
 
-    function increaseApproval(address _spender, uint _addedValue) public onlyOwner returns (bool) {
+    function increaseApproval(address _spender, uint _addedValue) public returns (bool) {
         allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
         Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
         return true;
     }
 
-    function decreaseApproval(address _spender, uint _subtractedValue) public onlyOwner returns (bool) {
+    function decreaseApproval(address _spender, uint _subtractedValue) public returns (bool) {
         uint oldValue = allowed[msg.sender][_spender];
         if (_subtractedValue > oldValue) {
             allowed[msg.sender][_spender] = 0;
@@ -144,7 +168,7 @@ contract LescovexERC20 is Ownable {
     }
 
     /* Approve and then communicate the approved contract in a single tx */
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public onlyOwner returns (bool success) {    
+    function approveAndCall(address _spender, uint256 _value, bytes _extraData) public returns (bool success) {    
         tokenRecipient spender = tokenRecipient(_spender);
 
         if (approve(_spender, _value)) {
@@ -156,7 +180,7 @@ contract LescovexERC20 is Ownable {
 
 
 interface tokenRecipient {
-    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) public ; 
+    function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData) external ; 
 }
 
     
@@ -174,10 +198,12 @@ contract Lescovex is LescovexERC20 {
     // constant to simplify conversion of token amounts into integer form
     uint256 public tokenUnit = uint256(10)**decimals;
 
+
     //Declare logging events
     event LogDeposit(address sender, uint amount);
     event LogWithdrawal(address receiver, uint amount);
   
+
     /* Initializes contract with initial supply tokens to the creator of the contract */
     function Lescovex() public {
         totalSupply = initialSupply;  // Update total supply
@@ -230,12 +256,24 @@ contract Lescovex is LescovexERC20 {
         return true;
     }
 
-    function withdrawReward() public status {
-        require (block.number - holded[msg.sender] > 172800); //1 month
+    function withdrawReward() public {
 
-        holded[msg.sender] = block.number;
-        uint256 ethAmount = tokenReward * balances[msg.sender];
+        uint i=0;
+        uint256 ethAmount;
+        uint256 len=holded[msg.sender].length;
 
+
+        while(i <= len-1){
+            if(block.number -  holded[msg.sender].time[i] > 5){
+                ethAmount += tokenReward * holded[msg.sender].amount[i];
+            }
+            i++;
+        }
+        
+
+        delete holded[msg.sender];
+
+        require(ethAmount>0);
         //send eth to owner address
         msg.sender.transfer(ethAmount);
           
@@ -243,7 +281,7 @@ contract Lescovex is LescovexERC20 {
         LogWithdrawal(msg.sender, ethAmount);
     }
 
-    function withdraw(uint value) public onlyOwner {
+    function withdraw(uint256 value) public onlyOwner {
         //send eth to owner address
         msg.sender.transfer(value);
 
@@ -266,9 +304,13 @@ contract Lescovex is LescovexERC20 {
 
         // SafeMath.add will throw if there is not enough balance.
         totalSupply = totalSupply.add(_value*2);
-        holded[_to] = block.number;
+
+        holded[_to].amount.push(_value);
+        holded[_to].time.push(block.number);
+        holded[_to].length++;
         balances[LescovexAddr] = balances[LescovexAddr].add(_value);
         balances[_to] = balances[_to].add(_value);
+
 
         Transfer(this, _to, _value);
         Transfer(this, LescovexAddr, _value);
