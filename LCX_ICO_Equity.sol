e0fc5ea1f9589dc8b08bf928043de5aafa61e2c7
rutrus <rutrus@gmail.com>
2018-03-10 05:34:44 +0100

    SafeMath.sub will throw if there is not enough balance.

--- LCX_ICO_Equity.sol
+++ LCX_ICO_Equity.sol
@@ -123,7 +123,6 @@ contract LescovexERC20 is Ownable {
     function transfer(address _to, uint256 _value) public returns (bool) {
         require(block.timestamp > blockEndICO || msg.sender == owner);
         require(_to != address(0));
-        require(_value <= balances[msg.sender]);
 
         // SafeMath.sub will throw if there is not enough balance.
         balances[msg.sender] = balances[msg.sender].sub(_value);
@@ -140,16 +139,16 @@ contract LescovexERC20 is Ownable {
 
     function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
         require(_to != address(0));
-        require(_value <= balances[_from]);
-        require(_value <= allowed[_from][msg.sender]);
+
+        // SafeMath.sub will throw if there is not enough balance.
         balances[_from] = balances[_from].sub(_value);
+        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
 
         delete holded[msg.sender];
         hold(_from,balances[_from]);
         hold(_to,_value);
 
         balances[_to] = balances[_to].add(_value);
-        allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
 
         Transfer(_from, _to, _value);
         return true;
