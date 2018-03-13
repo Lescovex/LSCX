137bca39114db0db1dabb6c143bc42abb827aa33
nOsDelAbismo <srdelabismo@lescovex.com>
2018-03-13 20:30:14 +0100

    testing files

---
+++ tests/truffle.js
@@ -0,0 +1,12 @@
+module.exports = {
+	networks:{
+		development:{
+		host: "localhost",
+		port:7545,
+		network_id:"*"
+		}
+	
+	}
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+};
