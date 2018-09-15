8bfb77aadc823c9c480a0208303b2b79fe207849
mireia <mipuco87@gmail.com>
2018-09-15 02:15:55 +0200

    Wallet - Withdraw Eths hidden on testnet

--- wallet/src/app/components/wallet/creditCard/creditCard.component.ts
+++ wallet/src/app/components/wallet/creditCard/creditCard.component.ts
@@ -392,6 +392,7 @@ export class CreditCardPage implements OnInit {
                 }, err => {},
             );
           }, 1000);
+          
       }
 
       checkAddress(receiverAddr): boolean {
