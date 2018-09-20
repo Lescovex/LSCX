30210c1febb7bb92ac87b004f7a0790b4ee6e635
mireia <mipuco87@gmail.com>
2018-09-20 17:49:33 +0200

    Lescovex DEX exe file

--- wallet/main.js
+++ wallet/main.js
@@ -8,46 +8,46 @@ const { WINDOW_OPTS, ENV } = require('./config/config.js');
 let win;
 
 function createWindow() {
-  // Create the browser window.
-  win = new BrowserWindow(WINDOW_OPTS);
+    // Create the browser window.
+    win = new BrowserWindow(WINDOW_OPTS);
 
-  // and load the index.html of the app.
-  win.loadURL(`file://${__dirname}/build/index.html`);
+    // and load the index.html of the app.
+    win.loadURL(`file://${__dirname}/build/index.html`);
 
-  // Open the DevTools.
-  if (ENV == 'dev') win.webContents.openDevTools()
+    // Open the DevTools.
+    //if (ENV == 'dev') win.webContents.openDevTools()
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', () => {
-  createWindow();
+    createWindow();
 })
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (win === null) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (win === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
