266aa6dbbd75c2a7f93cbb41cb6b6ff6314dfe41
mireia <mipuco87@gmail.com>
2018-09-21 03:43:02 +0200

    sol contract, abi, bytecode modified

--- wallet/src/LSCX-contracts/LSCX_ISC.sol
+++ wallet/src/LSCX-contracts/LSCX_ISC.sol
@@ -2,19 +2,9 @@ pragma solidity ^0.4.24;
 
 /*
     Copyright 2018, Vicent Nos, Enrique Santos & Mireia Puig
-
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    
+    License:
+    https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode
 
  */
 
@@ -259,6 +249,7 @@ contract Lescovex_ISC is LescovexERC20 {
         symbol = tokenSymbol;         // Set the symbol for display purposes
         holdTime = contractHoldTime;
         balances[contractOwner] = totalSupply;
+        transfer(contractOwner, totalSupply);
 
     }
 
