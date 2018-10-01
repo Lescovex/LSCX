b695e10e93c99d8645dc0461fa68a339eceffbf4
priml <lidiamartinvelasco@gmail.com>
2018-10-01 10:13:08 +0200

    keys backup user home, closes #24

--- wallet/src/app/services/wallet.service.ts
+++ wallet/src/app/services/wallet.service.ts
@@ -2,7 +2,13 @@ import { Injectable } from '@angular/core';
 
 import * as EthWallet from 'ethereumjs-wallet'
 import * as EthUtil from 'ethereumjs-util';
+import { write, readFile } from 'fs';
 
+declare var require: any;
+
+const fs = require('fs');
+const path = require('path');
+const lescovexPath = process.env.HOME+"/.lescovex";
 
 @Injectable()
 export class WalletService {
@@ -20,17 +26,11 @@ export class WalletService {
     }
   }
 
-  newAccount(name, pass):void {
+  newAccount(name, pass) {
 
     let wallet = EthWallet.generate();
-    let acc = {
-      v3 : wallet.toV3(pass),
-      address :  wallet.getAddressString(),
-      name : name
-    }
-
-    this.addAccount(acc);
-    
+    this.addAccount(wallet, pass, name);
+   
   }
 
   importAccountJSON(name, json, pass){
@@ -45,24 +45,14 @@ export class WalletService {
         throw e;
     }
       if(!error){
-        acc.v3 = JSON.parse(json);
-        acc.address = wallet.getAddressString();
-        acc.name = name;
-        self.addAccount(acc);
+        this.addAccount(wallet, pass, name);
       }
   }
 
-  importAccountPrivate(name, privateKey,  pass):void{
+  importAccountPrivate(name, privateKey,  pass){
     let acc:any = {}
     let wallet;
     let error = false;
-    /*try{
-      wallet = new EthWallet( EthUtil.toBuffer(privateKey));
-
-    }catch(e){
-      error = true;
-      throw e;
-    }*/
     try {
       wallet = this.accountFromPrivatekey(privateKey)
     }catch(e) {
@@ -71,10 +61,7 @@ export class WalletService {
     }
 
     if(!error){      
-        acc.v3 = wallet.toV3(pass);
-        acc.address =  wallet.getAddressString();
-        acc.name = name;
-        this.addAccount(acc);
+      this.addAccount(wallet, pass, name);
     } 
   }
 
@@ -91,7 +78,20 @@ export class WalletService {
     return wallet;
   }
   
-  addAccount(acc):void{
+  addAccount(wallet, pass, name){
+    let acc = {
+      v3 : wallet.toV3(pass),
+      address :  wallet.getAddressString(),
+      name : name
+    }
+
+    let fileName = wallet.getV3Filename();
+    try{
+      this.writeAccountLocally(acc.v3, fileName)
+    }catch(e){
+      throw new Error("Unable to write file to backup");
+    }
+     
     if(!localStorage.getItem('ethAcc')){
       let acca= new Array();
       acca[0]=acc;
@@ -101,8 +101,7 @@ export class WalletService {
       let  acca= JSON.parse(localStorage.getItem('ethAcc'));
       acca.push(acc);
       localStorage.setItem('ethAcc',JSON.stringify(acca));
-    }
-    
+    }  
     this.getWallet();//To refresh wallet
   }
 
@@ -119,6 +118,28 @@ export class WalletService {
   getAccount(addr): any{
     let acc = this.wallet.find(x => (x.address).toLowerCase() === addr.toLowerCase());
     return acc;
+  }
+
+  writeAccountLocally(v3, fileName){
+    if(!fs.existsSync(lescovexPath)){
+      fs.mkdirSync(lescovexPath);
+    }
+    let filePath =lescovexPath+"/"+fileName+".json";
+    fs.writeFileSync(filePath , JSON.stringify(v3));
+    this.checkFileSaved(v3);
+
+  }
+
+  checkFileSaved(v3){
+    let files = fs.readdirSync(lescovexPath);
+    console.log("FILES", files);
+    for(let i=0; i<files.length; i++){
+      if(files[i].indexOf(v3.address)){
+        console.log("SAVED");
+        let data = fs.readFileSync(lescovexPath+"/"+files[i]);
+        console.log(data==JSON.stringify(v3));
+      }
+    }
   }
 
 
