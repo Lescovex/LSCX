5fb9bcceaf603e4d8c227a15736248a3957f2f10
mireia <mipuco87@gmail.com>
2018-10-03 15:23:08 +0200

    copy&paste #39 mac bugs

--- wallet/main.js
+++ wallet/main.js
@@ -10,7 +10,7 @@ let win;
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow(WINDOW_OPTS);
-    //Menu.setApplicationMenu(Menu.buildFromTemplate(MENU_TEMPLATE));
+    Menu.setApplicationMenu(Menu.buildFromTemplate(MENU_TEMPLATE));
     // and load the index.html of the app.
     win.loadURL(`file://${__dirname}/build/index.html`);
 
